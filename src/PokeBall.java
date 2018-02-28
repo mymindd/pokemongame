3bcf06a8e8488a918d732f0c90340924e24f66e9
mymindd <mypspgood@gmail.com>
2018-03-01 00:15:00 +0700

    update

--- src/PokeBall.java
+++ src/PokeBall.java
@@ -1,20 +1,23 @@
 import java.io.*;
+import java.util.*;
+
 
 class PokeBall{
 	private float chance;
+    
+    public PokeBall(float chance){
+        this.chance = 0.8;
+    }
 
-
-	public void materBall(){
-		this.chance = .3f;
-	}
-	
-	public void grestBall(){
-		this.chance = .5f;
-	}
-
-	public void ultraBall(){
-		this.chance = .8f;
-	}
+    public boolean cath(){
+        Random rand = new Random();
+        int ch = rand.nextInt(100);
+        ch = ch * chance;
+        if (ch >= 50){
+            return true;
+        }
+        else return false;
+    }
 
 
 }
